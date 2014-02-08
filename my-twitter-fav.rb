17193ad114ec7d1e9a71862c39d98f299281548d
Max Borghino <fmborghino@gmail.com>
2014-02-08 13:57:51 -0800

    Switch to modular Sinatra style.

--- my-twitter-fav.rb
+++ my-twitter-fav.rb
@@ -1,63 +1,67 @@
 require 'yaml'
-require 'sinatra'
+require 'sinatra/base'
 require 'omniauth-twitter'
 
-$config = YAML.load_file(File.join(Dir.pwd, 'config.yml'))
-$consumer_key = ENV['TWITTER_CONSUMER_KEY'] || $config[:twitter_consumer_key]
-$consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || $config[:twitter_consumer_secret]
+class MyApp < Sinatra::Base
+  $config = YAML.load_file(File.join(Dir.pwd, 'config.yml'))
+  $consumer_key = ENV['TWITTER_CONSUMER_KEY'] || $config[:twitter_consumer_key]
+  $consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || $config[:twitter_consumer_secret]
 
-use OmniAuth::Builder do
-  provider :twitter, $consumer_key, $consumer_secret
-end
-
-configure do
-  enable :sessions
-end
-
-helpers do
-  def admin?
-    session[:admin]
+  use OmniAuth::Builder do
+    provider :twitter, $consumer_key, $consumer_secret
   end
-end
 
-get '/profile' do
-  halt(401,'Not Authorized') unless admin?
-  require 'twitter'
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key = $consumer_key
-    config.consumer_secret = $consumer_secret
-    config.oauth_token = session[:access_token]
-    config.oauth_token_secret = session[:access_token_secret]
+  configure do
+    enable :sessions
   end
-  "#{client.user.name}"
-end
 
-get '/public' do
-  "This is the public page - everybody is welcome!"
-end
+  helpers do
+    def admin?
+      session[:admin]
+    end
+  end
 
-get '/private' do
-  halt(401,'Not Authorized') unless admin?
-  "This is the private page - members only"
-end
+  get '/profile' do
+    halt(401,'Not Authorized') unless admin?
+    require 'twitter'
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = $consumer_key
+      config.consumer_secret = $consumer_secret
+      config.oauth_token = session[:access_token]
+      config.oauth_token_secret = session[:access_token_secret]
+    end
+    "#{client.user.name}"
+  end
 
-get '/login' do
-  redirect to("/auth/twitter")
-end
+  get '/public' do
+    "This is the public page - everybody is welcome!"
+  end
 
-get '/auth/twitter/callback' do
-  halt(401,'Not Authorized') if ! env['omniauth.auth']
-  session[:admin] = true
-  session[:access_token] = env['omniauth.auth']['credentials']['token']
-  session[:access_token_secret] = env['omniauth.auth']['credentials']['secret']
-  "<img src='#{env['omniauth.auth']['info']['image']}'> You are logged in #{env['omniauth.auth']['info']['name']}!"
-end
+  get '/private' do
+    halt(401,'Not Authorized') unless admin?
+    "This is the private page - members only"
+  end
 
-get '/auth/failure' do
-  params[:message]
-end
+  get '/login' do
+    redirect to("/auth/twitter")
+  end
 
-get '/logout' do
-  session[:admin] = nil
-  "You are now logged out"
+  get '/auth/twitter/callback' do
+    halt(401,'Not Authorized') if ! env['omniauth.auth']
+    session[:admin] = true
+    session[:access_token] = env['omniauth.auth']['credentials']['token']
+    session[:access_token_secret] = env['omniauth.auth']['credentials']['secret']
+    "<img src='#{env['omniauth.auth']['info']['image']}'> You are logged in #{env['omniauth.auth']['info']['name']}!"
+  end
+
+  get '/auth/failure' do
+    params[:message]
+  end
+
+  get '/logout' do
+    session[:admin] = nil
+    "You are now logged out"
+  end
+
+  run! if app_file == $0
 end
